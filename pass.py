4074a0c8735e8a34e1b76337c0cb53c91ffd83ba
vijayprathap <vijayprathap@users.noreply.github.com>
2013-11-05 15:45:13 +0100

    Update pass.py

--- pass.py
+++ pass.py
@@ -1,3 +1,8 @@
+import re
+
+num_check = re.compile(r"[0-9]")
+upp_case = re.compile(r"[A-Z]")
+spl_char = set('`~!@#$%^&*()-_=+{[}]|\;:"<,>.?/')
 password = raw_input("Enter your password: ")
 print password
 
@@ -8,16 +13,45 @@ def sixLetter(password):
 		return False
 		
 def hasNumber(password):
-	count=0
-	for j in password:
-		if j==str(0) or j==str(1) or j==str(2) or j==str(3) or j==str(4) or j==str(5) or j==str(6) or j==str(7) or j==str(8) or j==str(9):
-			count+=1
-		print count
-	print count	
-	if count>0:
+	val = num_check.search(password)
+	return bool(val)
+
+def hasUpper(password):
+	val = upp_case.search(password)
+	return bool(val)
+	
+def hasSplChar(password):
+	if any((c in spl_char) for c in password):
 		return True
 	else:
 		return False
-			
+		
+def notSameChar(password):
+	count = 0
+	for i in password:
+		for j in range(password.index(i)+1,len(password)):
+			if i is password[j]:
+				count+=1
+				c=password[j]
+	print count
+	if count>1:
+		print 'character %s is repeated twice' % c
+		return False
+	else:
+		return True
+		
+def checkPassword(password):
+	if not sixLetter(password) or not hasNumber(password) or not hasUpper(password) or not hasSplChar(password) or not notSameChar(password):
+		print "The given password is not good enough"
+		return True
+	else:
+		print "The given password is strong enough"
+		return False
+		
 print hasNumber(password)
 print sixLetter(password)
+print hasUpper(password)
+print hasSplChar(password)
+print notSameChar(password) 
+
+checkPassword(password)
