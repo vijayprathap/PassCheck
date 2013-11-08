acd352d04c4ad8e213945fb8de1280e84bac9f5a
vijayprathap <vijayprathap@users.noreply.github.com>
2013-11-08 18:18:26 +0100

    Update pass.py

--- pass.py
+++ pass.py
@@ -1,10 +1,12 @@
 import re
+import random
 
 num_check = re.compile(r"[0-9]")
 upp_case = re.compile(r"[A-Z]")
-spl_char = set('`~!@#$%^&*()-_=+{[}]|\;:"<,>.?/')
+spl_char = set('`~!@#$%^&*()-_=+{[}]|\;:"\'<,>.?/')
 password = raw_input("Enter your password: ")
 print password
+
 
 def sixLetter(password):
 	if len(password) >= 6:
@@ -27,20 +29,22 @@ def hasSplChar(password):
 		return False
 		
 def notSameChar(password):
-	count = 0
+	ismore=False
 	for i in password:
+		count=0
 		for j in range(password.index(i)+1,len(password)):
 			if i is password[j]:
 				count+=1
 				c=password[j]
-	print count
-	if count>1:
-		print 'character %s is repeated twice' % c
+			if count>1:
+				ismore=True
+	if ismore:
+		print 'character %s is repeated more than twice' % c
 		return False
 	else:
 		return True
 		
-def checkPassword(password):
+def isWeakPassword(password):
 	if not sixLetter(password) or not hasNumber(password) or not hasUpper(password) or not hasSplChar(password) or not notSameChar(password):
 		print "The given password is not good enough"
 		return True
@@ -48,10 +52,44 @@ def checkPassword(password):
 		print "The given password is strong enough"
 		return False
 		
-print hasNumber(password)
-print sixLetter(password)
-print hasUpper(password)
-print hasSplChar(password)
-print notSameChar(password) 
+def putNumber(password):
+	randindex = random.randint(0,len(password)-1)
+	randnum = random.randint(0,9)
+	newpass = password[:randindex] + str(randnum) + password[randindex:]
+	return newpass
+	
+def makeCaps(password):
+	randindex = random.randint(0,len(password)-1)
+	c=password[randindex]
+	while c.isdigit() or c in spl_char:
+		print 'test'
+		c=password[randindex]
+		print c
+		randindex = random.randint(0,len(password)-1)
+		#print randindex
+	print randindex
+	newpass = password[:randindex]+password[randindex].upper()+password[randindex+1:]
+	return newpass
+	
+def putSymbol(password):
+	randindex = random.randint(0,len(password)-1)
+	symbol=''.join(random.sample(spl_char,1))
+	newpass = password[:randindex] + symbol + password[randindex:]
+	return newpass
+	
+print makeCaps(password)
+#def putCharacter(password):
+	
+#print hasNumber(password)
+#print sixLetter(password)
+#print hasUpper(password)
+#print hasSplChar(password)
+#print notSameChar(password) 
 
-checkPassword(password)
+#isWeakPassword(password)
+
+#print putNumber(password)
+
+#print putSymbol(password)
+"""test = "my5space"
+print int(test[5])"""
