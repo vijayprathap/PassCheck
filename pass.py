9068627669fd0e77b129d3580707d357276547f1
vijayprathap <vijayprathap@users.noreply.github.com>
2013-11-11 12:05:15 +0100

    Update pass.py

--- pass.py
+++ pass.py
@@ -4,30 +4,36 @@ import random
 num_check = re.compile(r"[0-9]")
 upp_case = re.compile(r"[A-Z]")
 spl_char = set('`~!@#$%^&*()-_=+{[}]|\;:"\'<,>.?/')
-password = raw_input("Enter your password: ")
-print password
 
-
+#checks if the given password has atleast 6 characters
 def sixLetter(password):
 	if len(password) >= 6:
 		return True
 	else:
 		return False
-		
+
+
+#checks if the given password has a number		
 def hasNumber(password):
 	val = num_check.search(password)
 	return bool(val)
 
+
+#checks if the given password has a upper case character
 def hasUpper(password):
 	val = upp_case.search(password)
 	return bool(val)
+
 	
+#checks if the given password has a spl character
 def hasSplChar(password):
 	if any((c in spl_char) for c in password):
 		return True
 	else:
 		return False
-		
+
+
+#checks if a character in password is not repeated more than twice
 def notSameChar(password):
 	ismore=False
 	for i in password:
@@ -44,52 +50,72 @@ def notSameChar(password):
 	else:
 		return True
 		
+
+#checks if the given password is weak or strong
 def isWeakPassword(password):
-	if not sixLetter(password) or not hasNumber(password) or not hasUpper(password) or not hasSplChar(password) or not notSameChar(password):
-		print "The given password is not good enough"
+	if not sixLetters or not number or not hasUpperCase or not splChar or sameChar:
+		print "\nThe given password is not good enough"
 		return True
 	else:
-		print "The given password is strong enough"
+		print "\nThe given password is strong enough"
 		return False
 		
+#inserts a random number in the password string
 def putNumber(password):
 	randindex = random.randint(0,len(password)-1)
 	randnum = random.randint(0,9)
 	newpass = password[:randindex] + str(randnum) + password[randindex:]
 	return newpass
 	
+#changes a random character to uppercase
 def makeCaps(password):
 	randindex = random.randint(0,len(password)-1)
 	c=password[randindex]
 	while c.isdigit() or c in spl_char:
-		print 'test'
-		c=password[randindex]
-		print c
 		randindex = random.randint(0,len(password)-1)
-		#print randindex
-	print randindex
+		c=password[randindex]
 	newpass = password[:randindex]+password[randindex].upper()+password[randindex+1:]
 	return newpass
 	
+#inserts a spl character randomly
 def putSymbol(password):
 	randindex = random.randint(0,len(password)-1)
 	symbol=''.join(random.sample(spl_char,1))
 	newpass = password[:randindex] + symbol + password[randindex:]
 	return newpass
+
+#gives information about your password	
+def genReport(password):
+	print "\nThe given password has six characters: %s" %sixLetters
+	print "\nThe given password has a number: %s" %number
+	print "\nThe given password has an uppercase character: %s" %hasUpperCase
+	print "\nThe given password has a special character: %s" %splChar
+	print "\nThe given password has a character repeated more than twice: %s" %sameChar
 	
-print makeCaps(password)
-#def putCharacter(password):
+#gives you secure password
+def fixPassword(password):
+	if not number:
+		password=putNumber(password)
+	if not hasUpperCase:
+		password=makeCaps(password)
+	if not splChar:
+		password=putSymbol(password)
+	st = "\nThe suggested password is %s" %password
+	return st
+		
 	
-#print hasNumber(password)
-#print sixLetter(password)
-#print hasUpper(password)
-#print hasSplChar(password)
-#print notSameChar(password) 
+while True:
+	print "Password should be atleast 6 characters\n"
+	password = raw_input("Enter your password: ")
+	if sixLetter(password) and notSameChar(password):
+		break
+	
+sixLetters=sixLetter(password)
+number = hasNumber(password)
+hasUpperCase = hasUpper(password)
+splChar = hasSplChar(password)
+sameChar = not notSameChar(password)
 
-#isWeakPassword(password)
-
-#print putNumber(password)
-
-#print putSymbol(password)
-"""test = "my5space"
-print int(test[5])"""
+genReport(password)
+if isWeakPassword(password):
+	print fixPassword(password)
